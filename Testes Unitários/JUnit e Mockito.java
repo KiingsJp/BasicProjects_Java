6a13477adef33bc003eca3a95f9d4d84cef2e7f5
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-08-29 21:40:09 -0300

    Add files via upload

---
+++ Testes Unitários/JUnit e Mockito.java
@@ -0,0 +1,65 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// Com o Mockito os testes ja ficam interessantes
+// Consigo utilizar a anotação @Mock para instanciar um `dublê` para ser utilizado
+// Com isso preciso falar que essa classe pode ser injetada pelos mocos, com a anotação @InjectMocks
+// Como os Mocks são apenas dublês, precisamos dizer a eles o que eles precisam fazer, como eles devem agir e o que devem retornar.
+// exemplo: .willReturn(pet), estou dizendo para o duble retornar uma instância de pet que também é dublê 
+// também acesso o metodo do pet e digo que ele precisa me retornar true ou false, dependendo do resultado que eu quiser testar
+
+// Utilizado metodos do JUnit para testar um metodo void, assertThrows(tipoException, deOndeVemAException) e assertDoesNotThrow(deOndeVemAException)
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ValidacaoPetDisponivelTest {
+
+    @InjectMocks
+    private ValidacaoPetDisponivel validacao;
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    void deveriaPermitirSolicitacaoDeAdocaoPet(){
+
+        //ARRANGE
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+
+
+        //ASSERT + ACT
+        Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+
+    @Test
+    void naoDeveriaPermitirSolicitacaoDeAdocaoPet(){
+
+        //ARRANGE
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+
+
+        //ASSERT + ACT
+        Assertions.assertThrows(ValidacaoException.class,() -> validacao.validar(dto));
+    }
+
+}
---
+++ Testes Unitários/JUnit.java
@@ -0,0 +1,72 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Simplesmente foi criado um caso, de forma manual igual faço em uma classe Main, mas no lugar de um print foi usado o Assert do JUnit
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    @Test
+    void deveriaRetornarProbabilidadeAltaParaPetComIdadeBaixaEPesoBaixo(){
+        //idade 4 anos e 4kg - ALTA
+
+        //ARRANGE
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                4,
+                "Cinza",
+                4.0f
+        ), abrigo);
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+
+        //ACT
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+        //ASSERT
+        Assertions.assertEquals(ProbabilidadeAdocao.ALTA,probabilidade);
+    }
+
+    @Test
+    void deveriaRetornarProbabilidadeMediaParaPetComIdadeAltaEPesoBaixo(){
+        //idade 15 anos e 4kg - MEDIA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                15,
+                "Cinza",
+                4.0f
+        ), abrigo);
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+
+
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.MEDIA,probabilidade);
+    }
+
+
+
+}
